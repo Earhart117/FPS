c66e18fefe712c86f56fdf5fdce2b328a26f7904
Shaun <sme170330@utdallas.edu>
2020-10-02 17:53:14 -0500

    Initialization

---
+++ Project 02/Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+    private void Update()
+    {
+        //TO DO: bring up pop up menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitLevel();
+        }
+        //increase score
+        //TO DO: replace w implementation
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
+    }
+    public void ExitLevel()
+    {
+        //compare score to high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            //save current score as new high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+        //load main menu
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void IncreaseScore(int scoreIncrease)
+    {
+        //increase score
+        _currentScore += scoreIncrease;
+        //update screen display
+        _currentScoreTextView.text = "Score: " + _currentScore.ToString();
+    }
+}
---
+++ Project 02/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
+
+
+    //Start is called before first frame update
+    private void Start()
+    {
+        //load high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+        // play song at start
+        if(_startingSong !=null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+}
